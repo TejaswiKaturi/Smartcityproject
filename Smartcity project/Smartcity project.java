f483013c0911d074f331c4ac8ebeae1b9f1260e8
TejaswiKaturi <121794679+TejaswiKaturi@users.noreply.github.com>
2023-03-17 13:57:38 +0530

    Add files via upload

---
+++ Smartcity project/Smartcity project.java
@@ -0,0 +1,121 @@
+package project;
+import java.util.Scanner;
+
+
+public class Smartcity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+int choice = 0;
+while(true) {
+	System.out.println("Welcome to the Smartcity Project!please select an option:");
+	System.out.println("1.Hotels");
+	System.out.println("2.Transportation facilities");
+	System.out.println("3.Air ticket booking");
+	System.out.println("4.Shopping Details");
+	System.out.println("5.City news");
+	System.out.println("6.Exit");
+ choice = sc.nextInt();
+ switch(choice) {
+ case 1:
+	 System.out.println("Hotels:Here are some recommended hotels in the city....");
+	 break;
+	 
+	 case 2:
+		 System.out.println("Transportation facilities: Here are some available transportation options...");
+		 break;
+
+	 
+	 case 3:
+		 System.out.println("Air tickets booking: Here are some airlines that fly to/from the city ...");
+		 break;
+	 
+	 case 4:
+		 System.out.println("Shopping details: Here are some popular shopping destination in the city ...");
+		 break;
+		 
+	 case 5:
+		 System.out.println("City news: Here are the latest news and events in the city ...");
+		 break;
+		 
+		 
+	 case 6:
+		 System.out.println("Exiting smartcity project ...");
+		 System.exit(0);
+		 break;
+		 
+		 default:
+			 System.out.println("Invalid choice.please try again..");
+			 break;
+			 
+	 
+	 
+	 
+ }
+
+
+	
+	
+	
+	
+	
+}
+	}
+
+}
+output:
+Welcome to the Smartcity Project!please select an option:
+1.Hotels
+2.Transportation facilities
+3.Air ticket booking
+4.Shopping Details
+5.City news
+6.Exit
+1
+Hotels:Here are some recommended hotels in the city....
+Welcome to the Smartcity Project!please select an option:
+1.Hotels
+2.Transportation facilities
+3.Air ticket booking
+4.Shopping Details
+5.City news
+6.Exit
+2
+Transportation facilities: Here are some available transportation options...
+Welcome to the Smartcity Project!please select an option:
+1.Hotels
+2.Transportation facilities
+3.Air ticket booking
+4.Shopping Details
+5.City news
+6.Exit
+3
+Air tickets booking: Here are some airlines that fly to/from the city ...
+Welcome to the Smartcity Project!please select an option:
+1.Hotels
+2.Transportation facilities
+3.Air ticket booking
+4.Shopping Details
+5.City news
+6.Exit
+4
+Shopping details: Here are some popular shopping destination in the city ...
+Welcome to the Smartcity Project!please select an option:
+1.Hotels
+2.Transportation facilities
+3.Air ticket booking
+4.Shopping Details
+5.City news
+6.Exit
+5
+City news: Here are the latest news and events in the city ...
+Welcome to the Smartcity Project!please select an option:
+1.Hotels
+2.Transportation facilities
+3.Air ticket booking
+4.Shopping Details
+5.City news
+6.Exit
+6
+Exiting smartcity project ...
